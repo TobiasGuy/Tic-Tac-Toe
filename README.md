96af743621081e3b86cd4fe5a29720978a929373
TobiasGuy <tobias.guy.tobo@gmail.com>
2024-06-11 16:51:35 +0100

    Initial plan
    Initialized repo. Broke down the program into smaller problems and wrote each of them out

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Tic-Tac-Toe
  Basic python tic-tac-toe recreation
+
+#### 2024-06-11
+# 1
+Initial commit done, repo made
+TODO: create initial plan (done)
---
+++ game.py
@@ -0,0 +1,20 @@
+# Display 'board' that you're playing on
+# Do this with coordinates? or 1-9? Maybe for bigger scale coordinates would be good but might not be necessary for this
+# Choose between 1 array vs multi-dimensional array to store values
+# Prompt user to choose between single player (vs ai), or 2 player
+# Validate input, make sure it's good, if not then reprompt
+# Create ai enemy 
+# Prompt user for input for choosing position
+# Validate input, make sure it's good, if not then reprompt
+# Update 'board' with new inputs and display
+# Check for 3 in a row
+# Check for whole board being full
+# Keep track of who's turn it is, X and O respectively
+# After game is done, print winner, prompt user asking if they want to play again or exit
+
+
+# Features for later:
+# Later add a scoreboard?
+# Username?
+# Add an option to access the scoreboard when selecting single or multi-player
+# Add a proper UI instead of just using the terminal?
