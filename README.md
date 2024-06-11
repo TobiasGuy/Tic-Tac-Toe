c48055d3d91fe5a9d4171f46c21522e4d0c63349
TobiasGuy <tobias.guy.tobo@gmail.com>
2024-06-11 19:03:00 +0100

    Initialising Functions
    Reorganised comments in game.py to put sections in order
    Created functions that I might need for each section

--- README.md
+++ README.md
@@ -9,3 +9,7 @@ TODO: create initial plan (done)
 # 2
 Finalised initial plan
 Copied plan into new file plan.md so it doesn't get lost and can be referenced later when the code gets messier
+
+# 3
+Reorganised comments in game.py to put sections in order
+Created functions that I might need for each section
--- game.py
+++ game.py
@@ -1,45 +1,65 @@
-# Display 'board' that you're playing on
-    # Probably make a function for this
-    # Display after prompt, before win/loss, and reprompt if the game is not over
+# Import libraries if necessary:
+import random
 
-# Do this with coordinates? or 1-9? Maybe for bigger scale coordinates would be good but might not be necessary for this
-# Choose between 1 array vs multi-dimensional array to store values
-    # I'll use a 2d array think that might make things nicer
+# Variables to initialise:
+gamemode = 0 # Use 0, 1, 2 for main menu, single player, multi-player respectively
+player_turn = ""
 
-# Prompt user to choose between single player (vs ai), or 2 player
-# Validate input, make sure it's good, if not then reprompt
-    # Should be pretty simple
-    # Type 1 for single, 2 for multi
-    # Check that user input matches one of the inputs you want
+def main():
+    # Prompt user to choose between single player (vs ai), or 2 player, or let them exit
+    # Validate input, make sure it's good, if not then reprompt
+        # Should be pretty simple
+        # Type 1 for single, 2 for multi, q for quit?
+        # Check that user input matches one of the inputs you want
 
-# Create ai enemy 
-    # Only active when it's single player
-    # Probably make a variable that changes when you want it to be active or not.
-    # Remember to reset this value on new game -- maybe make a list of values to reset?
+    # Prompt user for input for choosing position
+    # Validate input, make sure it's good, if not then reprompt
+        # Input position between 1-9 (maybe coordinates didn't decide yet)
+        # Check that the square doesn't already have an X or O in it!
+    return
 
-# Prompt user for input for choosing position
-# Validate input, make sure it's good, if not then reprompt
-    # Input position between 1-9 (maybe coordinates didn't decide yet)
-    # Check that the square doesn't already have an X or O in it!
+def display_board():
+    # Display 'board' that you're playing on
+        # Probably make a function for this
+        # Display after prompt, before win/loss, and reprompt if the game is not over
+        # Do this with coordinates? or 1-9? Maybe for bigger scale coordinates would be good but might not be necessary for this
+        # Choose between 1 array vs multi-dimensional array to store values
+            # I'll use a 2d array think that might make things nicer
+    return
 
-# Update 'board' with new inputs and display
-    # Store X or O depending on player's turn
-    # Randomise on game start? Also display who's turn it currently is
+def ai_move():
+    # Create ai enemy 
+        # Only active when it's single player
+        # Probably make a variable that changes when you want it to be active or not.
+        # Remember to reset this value on new game -- maybe make a list of values to reset?
+        # Make sure ai actually moves on its turn
+    return
 
-# Check for 3 in a row
-    # Probably a function for checking each direction: Vertical, Horizontal, Diagonal
+def board_update():
+    # Update 'board' with new inputs and display
+        # Store X or O depending on player's turn
+        # Randomise on game start? Also display who's turn it currently is
+    return
 
-# Check for whole board being full
-    # Loop through each position and if they're all full, call a draw
+def check_board():
+    # Check for 3 in a row
+        # Probably a function for checking each direction: Vertical, Horizontal, Diagonal
+    # Check for whole board being full
+        # Loop through each position and if they're all full, call a draw
+    return
 
-# Keep track of who's turn it is, X and O respectively
-    # Have a variable that switches after each turn is taken
+def switch_turn():
+    # Keep track of who's turn it is, X and O respectively
+        # Have a variable that switches after each turn is taken
+    return
 
-# After game is done, print winner, prompt user asking if they want to play again or exit
-    # Maybe a big while true loop only for this reason that breaks if you want to exit and continues otherwise?
+def game_end():
+    # After game is done, print winner, prompt user asking if they want to play again or exit
+        # Maybe a big while true loop only for this reason that breaks if you want to exit and continues otherwise?
+    return
 
 
-
+main()
 
 # Features for later:
     # Leaving these things alone for now
